ccd30568e522e91588d375ca59791052035bf713
Fernando Freije <damperfreije@hotmail.com>
2017-05-04 20:46:37 +0200

    Added new participants project

---
+++ participants_i3a/src/main/java/es/uniovi/asw/domain/User.java
@@ -0,0 +1,193 @@
+package es.uniovi.asw.domain;
+
+import java.sql.Date;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity(name = "Users")
+public class User {
+
+	@Id
+	private Long id;
+	private String login;
+	private String password;
+	private String firstname;
+	private String lastname;
+	private String address;
+	private String nationality;
+	private String DNI;
+	private Date birthday;
+
+	public User() {
+	}
+
+	public User(Long id, String login, String password, String firstname, String lastname, String address,
+			String nationality, String dNI, Date birthday) {
+		super();
+		this.id = id;
+		this.login = login;
+		this.password = password;
+		this.firstname = firstname;
+		this.lastname = lastname;
+		this.address = address;
+		this.nationality = nationality;
+		this.DNI = dNI;
+		this.birthday = birthday;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getLogin() {
+		return login;
+	}
+
+	public void setLogin(String login) {
+		this.login = login;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getFirstName() {
+		return firstname;
+	}
+
+	public void setFirstName(String firstname) {
+		this.firstname = firstname;
+	}
+
+	public String getLastName() {
+		return lastname;
+	}
+
+	public void setLastName(String lastname) {
+		this.lastname = lastname;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getNationality() {
+		return nationality;
+	}
+
+	public void setNationality(String nationality) {
+		this.nationality = nationality;
+	}
+
+	public String getDNI() {
+		return DNI;
+	}
+
+	public void setDNI(String dNI) {
+		DNI = dNI;
+	}
+
+	public Date getBirthday() {
+		return birthday;
+	}
+
+	public void setBirthday(Date birthday) {
+		this.birthday = birthday;
+	}
+
+	public String toStringLogin() {
+		return "User [" + login + "," + password + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((DNI == null) ? 0 : DNI.hashCode());
+		result = prime * result + ((address == null) ? 0 : address.hashCode());
+		result = prime * result + ((birthday == null) ? 0 : birthday.hashCode());
+		result = prime * result + ((firstname == null) ? 0 : firstname.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((lastname == null) ? 0 : lastname.hashCode());
+		result = prime * result + ((login == null) ? 0 : login.hashCode());
+		result = prime * result + ((nationality == null) ? 0 : nationality.hashCode());
+		result = prime * result + ((password == null) ? 0 : password.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		if (DNI == null) {
+			if (other.DNI != null)
+				return false;
+		} else if (!DNI.equals(other.DNI))
+			return false;
+		if (address == null) {
+			if (other.address != null)
+				return false;
+		} else if (!address.equals(other.address))
+			return false;
+		if (birthday == null) {
+			if (other.birthday != null)
+				return false;
+		} else if (!birthday.equals(other.birthday))
+			return false;
+		if (firstname == null) {
+			if (other.firstname != null)
+				return false;
+		} else if (!firstname.equals(other.firstname))
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (lastname == null) {
+			if (other.lastname != null)
+				return false;
+		} else if (!lastname.equals(other.lastname))
+			return false;
+		if (login == null) {
+			if (other.login != null)
+				return false;
+		} else if (!login.equals(other.login))
+			return false;
+		if (nationality == null) {
+			if (other.nationality != null)
+				return false;
+		} else if (!nationality.equals(other.nationality))
+			return false;
+		if (password == null) {
+			if (other.password != null)
+				return false;
+		} else if (!password.equals(other.password))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", login=" + login + ", password=" + password + ", firstName=" + firstname
+				+ ", lastName=" + lastname + ", address=" + address + ", nationality=" + nationality + ", DNI=" + DNI
+				+ ", birthday=" + birthday + "]";
+	}
+}
---
+++ participants_i3a/src/test/java/es/uniovi/asw/domain/UserTest.java
@@ -0,0 +1,71 @@
+package es.uniovi.asw.domain;
+
+import static org.junit.Assert.*;
+
+import java.sql.Date;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import es.uniovi.asw.DateConversor;
+import es.uniovi.asw.domain.User;
+
+public class UserTest {
+
+	private User user1;
+	private User user2;
+
+	@Before
+	public void setUp() throws Exception {
+		user1 = new User(1L, "dgsama", "1234", "David", "Garcia Gonzalez", "C/Lucio Villegas", "Spanish", "12345678Z",
+				DateConversor.createSqlDate("14-01-1995"));
+		user2 = new User(1L, "kingInNorth", "gh0st", "John", "Snow", "Winterfell", "Northerm", "01234567A",
+				DateConversor.createSqlDate("04-09-1980"));
+	}
+
+	@Test
+	public void gettersAndSettersTest() {
+
+		assertEquals(user1.getBirthday(), DateConversor.createSqlDate("14-01-1995"));
+
+		assertTrue(user1.getFirstName().toLowerCase().equals("david"));
+		user1.setFirstName("Juan");
+		assertFalse(user1.getFirstName().toLowerCase().equals("david"));
+
+		assertTrue(user2.getPassword().equals("gh0st"));
+		user2.setPassword("st4rk");
+		assertEquals(user2.toStringLogin(), "User [kingInNorth,st4rk]");
+
+		user2.setAddress("North of the wall");
+		user2.setBirthday(DateConversor.createSqlDate("04-09-1800"));
+
+	}
+
+	@Test
+	public void compareTest() {
+
+		assertFalse(user1.equals(user2));
+
+		user2.setId(1L);
+		user2.setLogin("dgsama");
+		user2.setPassword("1234");
+		user2.setFirstName("David");
+		user2.setLastName("Garcia Gonzalez");
+		user2.setAddress("C/Lucio Villegas");
+		user2.setNationality("Spanish");
+		user2.setDNI("12345678Z");
+		user2.setBirthday(DateConversor.createSqlDate("14-01-1995"));
+
+		assertTrue(user1.equals(user2));
+	}
+	
+	
+	@Test
+	public void testEquals_Symmetric() {
+	    Assert.assertTrue(user1.equals(user1) && user1.equals(user1));
+	    Assert.assertTrue(user1.hashCode() == user1.hashCode());
+	}
+
+
+}
